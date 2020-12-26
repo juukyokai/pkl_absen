91617722cded641b227409c69821d761b0b56320
JuukyoKai <juukyokai00@gmail.com>
2020-12-26 16:21:47 +0700

    sql kampret

--- back-end/load_tables.php
+++ back-end/load_tables.php
@@ -12,7 +12,8 @@
                         mata_kuliah.sks,
                         mata_kuliah.status_mk
                     FROM kelas, mata_kuliah
-                    where kelas.id_mk=mata_kuliah.id_mk";
+                    WHERE kelas.id_mk=mata_kuliah.id_mk
+                    ORDER BY kelas.id_kelas";
                     
     $result = $conn->query($sql_kelas);
     $iterasi = 1;
@@ -26,11 +27,11 @@
             <td> <span class='count'>". $row_kelas['sks'] ."</span> </td>
             <td> <span class='name'>Cak Prat</span> </td>
             <td><span class='product'>". $row_kelas['hari_kelas'] ."</span></td>
-            <td><span class='>". $row_kelas['jam_kelas'] ."</span></td>
+            <td><span class=>". $row_kelas['jam_kelas'] ."</span></td>
             <td>");
-            if($row_kelas['status']==1){
+            if($row_kelas['status_mk']==1){
                 echo("<span class='badge badge-pending'>Active</span>");
-            }else if($row_kelas['status']==2){
+            }else if($row_kelas['status_mk']==2){
                 echo("<span class='badge badge-complete'>Complete</span>");
             }else{
                 echo("<span class='badge badge-pending'>Error</span>");
