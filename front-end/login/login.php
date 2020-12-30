25f99a24f843bf9c0f794673523faaedd90f7e3d
JuukyoKai <juukyokai00@gmail.com>
2020-12-30 21:13:00 +0700

    configure login

---
+++ front-end/login/login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Login Kelas Daring</title>
+  <link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<div class="login-page">
+  <div class="form">
+    <form class="login-form">
+      <input type="text" placeholder="Username"/>
+      <input type="password" placeholder="Password"/>
+      <button>login</button>
+      <p class="message">Tidak Dapat Masuk? Narahubung 082217401318</p>
+      <p></p>
+      <p class="goback"> <a href="../index.html">Kembali ke Landing Page</a></p>
+    </form>
+  </div>
+</div>
+<!-- partial -->
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+  <script  src="./script.js"></script>
+</body>
+</html>
