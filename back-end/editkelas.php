3fd369a67be01f2e62c0734b66acb68d7c223914
Elang-Eka <elangputra87@gmail.com>
2021-01-06 11:53:12 +0700

    fixing

---
+++ back-end/editkelas.php
@@ -0,0 +1,16 @@
+<?php
+    include('db_connect.php');
+		if(!empty($_POST))
+		{
+			$id_mk = $_POST['id_mk'];
+			$id_dosen = $_POST['id_dosen'];
+			$kode_nama_kelas = $_POST['kode_nama_kelas'];
+			$hari_kelas = $_POST['hari_kelas'];
+			$jam_kelas = $_POST['jam_kelas'];
+			$query = " INSERT INTO kelas(id_mk, id_dosen, kode_nama_kelas, hari_kelas, jam_kelas)  
+			 VALUES('$id_mk','$id_dosen','$kode_nama_kelas','$hari_kelas','$jam_kelas')
+			";
+			$req = $conn->query($query);
+			echo $output;
+		}
+?>
